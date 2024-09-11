9ec11c81cf5824675dd6fce48b03a658e51b4cdd
Klevis <klevis.maze@gmail.com>
2024-09-12 00:08:28 +0200

    login and register pages

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import { useState } from "react";
+import "./App.css";
+import Router from "./router/Router";
+
+function App() {
+  const [allRoutes, setAllRoutes] = useState([])
+  return <Router allRoutes={allRoutes} />
+}
+
+export default App;
